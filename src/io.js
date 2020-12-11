79cc506dcbb409d67b4395d89be8867a1e03ee45
Xavier Murcia <xavier.murcia@wefoxgroup.com>
2020-12-11 19:20:19 +0100

    Change readme title and fix console.log lint

--- src/io.js
+++ src/io.js
@@ -2,6 +2,7 @@ import { io } from 'socket.io-client';
 
 const socket = io('http://localhost:3000');
 
+// eslint-disable-next-line no-console
 socket.on('connected', () => console.log('is connected'));
 
 export default socket;
